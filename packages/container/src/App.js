7678dbaccf02a1c3278af69be365c7a44ddaaa4f
HKHALDI <hkhaldi@ford.com>
2023-11-10 22:01:32 -0500

    added invalidation through build

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,5 +2,10 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
-  return <MarketingApp />;
+  return (
+    <>
+      <h1>Hi Ther !</h1>
+      <MarketingApp />
+    </>
+  );
 };
