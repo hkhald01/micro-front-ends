b04054a659975d1d3ce5c5029bf303210ece805e
HKHALDI <hkhaldi@ford.com>
2023-11-11 09:48:50 -0500

    fixed css issue

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'cn',
+});
 export default () => {
   return (
     <BrowserRouter>
-      <Header />
-      <MarketingApp />
+      <StylesProvider generateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
   );
 };
